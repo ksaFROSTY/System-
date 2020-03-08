5f9d60f2b206b03db24a6b7d5f3dcbe8cdc8e831
ksaFROSTY <59263338+ksaFROSTY@users.noreply.github.com>
2020-03-08 03:05:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -384,7 +384,7 @@ if (message.content.startsWith(prefix + 'help')) { //DiamondCodes - [ X_KillerYT
 『welcome/ مثل بروبوت اسم روم ولكم』
 『!togglelog/لتفعيل اللوق』
 『!setLog/تحديد روم لوق』
-『?rps / لعبة حجرة ورقة مقص 』
+『 / لعبة حجرة ورقة مقص 』
 『?اسئلة للعبة فورت نايت /  فورت نايت 』
 **
    
@@ -714,6 +714,80 @@ if(!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply('** Yo
 });
 
 //clear
+
+client.on('message', message => {  
+    if (message.author.bot) return; 
+    if (message.content.startsWith(prefix + 'clear')) { 
+    if(!message.channel.guild) return message.reply(`** This Command For Servers Only**`); 
+     if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send(`** You don't have Premissions!**`);
+     if(!message.guild.member(client.user).hasPermission('MANAGE_GUILD')) return message.channel.send(`**I don't have Permission!**`);
+    let args = message.content.split(" ").slice(1)
+    let messagecount = parseInt(args);
+    if (args > 100) return message.reply(`** The number can't be more than **100** .**`).then(messages => messages.delete(5000))
+    if(!messagecount) args = '100';
+    message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages)).then(msgs => {
+    message.channel.send(`** Done , Deleted `${msgs.size}` messages.**`).then(messages => messages.delete(5000));
+    })
+  }
+});
+
+//color
+
+client.on("message", message => {
+    if (message.content == `${prefix}colors`) {
+        var fsn = require('fs-nextra');
+        fs.readdir('./img', async (err, files) => {
+            var f = files[Math.floor(Math.random() * files.length)];
+            var {
+                Canvas
+            } = require('canvas-constructor');
+            var x = 0;
+            var y = 0;
+             if (message.guild.roles.filter(role => !isNaN(role.name)).size <= 0) return message.reply("can't find any colors")
+            message.guild.roles.filter(role => !isNaN(role.name)).sort((b1, b2) => b1.name - b2.name).forEach(() => {
+                x += 100;
+                if (x > 100 * 12) {
+                    x = 100;
+                    y += 80;
+                }
+            });
+            var image = await fsn.readFile(`./img/${f}`);
+            var xd = new Canvas(100 * 11, y + 250)
+                .addBeveledImage(image, 0, 0, 100 * 11, y + 250, 50)
+                .setTextBaseline('middle')
+                .setColor('white')
+                .setTextSize(60)
+                .addText(`Colors list : ${message.guild.name}`, 140, 40);
+            x = 0;
+            y = 150;
+            message.guild.roles.filter(role => !isNaN(role.name)).sort((b1, b2) => b1.name - b2.name).forEach(role => {
+                x += 75;
+                if (x > 100 * 10) {
+                    x = 75;
+                    y += 80;
+                }
+                xd
+                    .setTextBaseline('middle')
+                    .setTextAlign('center')
+                    .setColor(role.hexColor)
+                    .addBeveledRect(x, y, 60, 60, 15)
+                    .setColor('white');
+                if (`${role.name}`.length > 2) {
+                    xd.setTextSize(30);
+                } else if (`${role.name}`.length > 1) {
+                    xd.setTextSize(40);
+                } else {
+                    xd.setTextSize(50);
+                }
+                xd.addText(role.name, x + 30, y + 30);
+            });
+            message.channel.sendFile(xd.toBuffer());
+        });
+    }
+})
+
+
+
 
 
 
